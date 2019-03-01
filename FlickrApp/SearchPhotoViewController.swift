0e6549b30dd77c52a230abb2eefc2474ea251c7b
Haoxin Li <54lihaoxin@gmail.com>
2019-03-01 15:31:48 -0800

    initiate the root view programmatically

---
+++ FlickrApp/SearchPhotoViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SearchPhotoViewController.swift
+//  FlickrApp
+//
+//  Created by Haoxin Li on 3/1/19.
+//  Copyright © 2019 Haoxin Li. All rights reserved.
+//
+
+import FlickrFoundation
+import UIKit
+
+final class SearchPhotoViewController: UIViewController {}
